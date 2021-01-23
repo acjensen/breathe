350e8d02637ff07294890d7ff214be9cd2ce2553
Andrew Jensen <andrew.charles.jensen@gmail.com>
2021-01-22 22:01:46 -0600

    Main page and about page done. needs animation, more css, heartbeat

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Installation
 
 1. Build the docker image.
-`docker build .`
+`docker build -t breathe:breathe .`
 
 2. Run the container.
-`docker run -p 8080:8080 --name breathe --env PORT=8080 2b75e2568928`
+`docker run -p 8080:8080 --name breathe --env PORT=8080 breathe:breathe`
 
 3. Visit `http://localhost:8080/`.
--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
 @app.route('/')
 def main():
-    return "Lol!"
+    return render_template('main.html', words='This is the main page.')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
